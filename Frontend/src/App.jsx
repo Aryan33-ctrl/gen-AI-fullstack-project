572506888dcd1766bc57d2df8e216d268ce15cbe
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-08-01 22:47:44 +0530

    Make changes to UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
-
+import {RouterProvider}from "react-router";
+import {router}from 
 
 
 function App() {
--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login=()=>{
     return(
-        <div>Login</div>
+        <div>Login Here</div>
     )
 }
 export default Login
