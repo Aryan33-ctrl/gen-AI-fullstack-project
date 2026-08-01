e0ca004a2660cc2b34e61539684fa92902812c6f
Aryan Dixit <aryan.dixit_cs24@gla.ac.in>
2026-08-01 23:19:26 +0530

    Make changes to UI

--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const Login=()=>{
-    return(
-        <div>Login Here</div>
-    )
+const login = () => {
+  return (
+    <main>
+        <div className="login-container">
+            <h1>Login</h1>
+            <form>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" required/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required/>     
+                </div>
+                <button className="button primary-button" >Login</button>
+            
+            </form>
+        </div>
+    </main>
+  )
 }
-export default Login
+
+export default login
